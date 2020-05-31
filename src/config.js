f3b305071b1bf0c3cf270cc32e4e8ea1ae62d236
Manikumar K V <mkanamarlapudi@nasinsurance.com>
2020-06-01 01:48:34 +0530

    added host

--- src/config.js
+++ src/config.js
@@ -1,21 +1,21 @@
 export default {
-    s3: {
-      REGION: "us-east-1",
-      BUCKET: "sls-online-test-public"
-    },
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "http://localhost:3000/dev"
-      
-      //URL:"https://svtj4fpsz2.execute-api.us-east-1.amazonaws.com/dev",
-      // URL:"https://t5xovlkc3f.execute-api.us-east-1.amazonaws.com/dev",
-      // URL: "https://czla69q7ql.execute-api.us-east-1.amazonaws.com/dev"
-    },
-    cognito: {
-      REGION: "us-east-1",
-      USER_POOL_ID: "us-east-1_coVPXKLPD",
-      APP_CLIENT_ID: "4roh06r403pci87qfo0j6ipuks",
-      IDENTITY_POOL_ID: "us-east-1:f59389b7-b99d-4da9-95b8-185eb0353608"
-    },
-    MAX_ATTACHMENT_SIZE:1000000
-  };
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "sls-online-test-public"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    // URL: "http://localhost:3000/dev"
+    URL: 'https://svtj4fpsz2.execute-api.us-east-1.amazonaws.com/dev'
+    //URL:"https://svtj4fpsz2.execute-api.us-east-1.amazonaws.com/dev",
+    // URL:"https://t5xovlkc3f.execute-api.us-east-1.amazonaws.com/dev",
+    // URL: "https://czla69q7ql.execute-api.us-east-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_coVPXKLPD",
+    APP_CLIENT_ID: "4roh06r403pci87qfo0j6ipuks",
+    IDENTITY_POOL_ID: "us-east-1:f59389b7-b99d-4da9-95b8-185eb0353608"
+  },
+  MAX_ATTACHMENT_SIZE: 1000000
+};
